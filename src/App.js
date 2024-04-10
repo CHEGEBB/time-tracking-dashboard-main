6ee0041590184fcc1ad4202db5d8f3f31135b032
CHEGEBB <chegephil24@gmail.com>
2024-04-10 14:10:53 +0300

    js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ function App() {
   const profilePic = require('./images/image-jeremy.png');
   return (
     <div className="App">
+    <div className="tracking-dashboard">
     <div className="profile">
       <img src={profilePic} alt="profile" />
     </div>
-      <h1>Report for</h1>
+    <div className="tracking-content">
+    <h1>Report for</h1>
+    <h2>Jeremy Robson</h2>
+    </div>
+    </div>
+   
+      
     </div>
   );
 }
