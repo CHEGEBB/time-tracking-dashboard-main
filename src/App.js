c198daadf835e2c149872f00177f97d394f87b72
CHEGEBB <chegphil24@gmail.com>
2024-04-10 12:45:30 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import './index.scss';
+import Dashboard from './components/dashboard'; 
+import './components/dashboard.scss';
 
 function App() {
   return (
     <div className="App">
+      <Dashboard /> {/* Use correct case for component */}
     </div>
   );
 }
