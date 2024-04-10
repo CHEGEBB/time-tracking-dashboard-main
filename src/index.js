e9a2068e5b6bc7cd7e62ae42e9171c25ddd7f130
CHEGEBB <chegephil24@gmail.com>
2024-04-10 13:51:51 +0300

    new

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import './components/dashboard.scss';
-import './components/dashboard';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
