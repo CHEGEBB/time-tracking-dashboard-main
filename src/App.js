29346191ad3a3131991fa3ef3d8d50e461819726
CHEGEBB <chegephil24@gmail.com>
2024-04-10 14:02:53 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
     <div className="profile">
-      <img src="https://avatars.githubusercontent.com/u/1?v=4" alt="profile" />
-      <h1>Linus Torvalds</h1>
-      <p>Software Engineer</p>
+      <img src="./images/image-jeremy.png" alt="profile" />
     </div>
       <h1>Report for</h1>
     </div>
