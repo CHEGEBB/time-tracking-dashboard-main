5c7aaa04749cd3cbd202363ed39673c5b46dfc79
CHEGEBB <chegephil24@gmail.com>
2024-04-11 11:40:31 +0300

    jsx

--- src/components/InfoComponent.js
+++ src/components/InfoComponent.js
@@ -1,34 +1,29 @@
-// InfoComponent.js
-
-import React, { useState } from 'react';
+import React from 'react';
 import Profilepic from "../images/image-jeremy.png";
 import SideNav from "./SideNav";
 import "../InfoComponent.scss";
 
-const InfoComponent = () => {
-  const [selectedTimeframe, setSelectedTimeframe] = useState('Daily');
-
+const InfoComponent = ({ selectedTimeframe, setSelectedTimeframe }) => {
   const handleNavClick = (option) => {
     setSelectedTimeframe(option);
   };
 
   return (
     <div className="info-back">
-    <div className="info">
-    <div className="profile">
-        <img src={Profilepic} alt="profile" />
-      </div>
-      <div className="info-content">
-        <div className="profile-info">
-          <h1>Report for</h1>
-          <h2>Jeremy Robson</h2>
+      <div className="info">
+        <div className="profile">
+          <img src={Profilepic} alt="profile" />
         </div>
-        <div className="side-nav">
-          <SideNav onNavClick={handleNavClick} selectedOption={selectedTimeframe} />
+        <div className="info-content">
+          <div className="profile-info">
+            <h1>Report for</h1>
+            <h2>Jeremy Robson</h2>
+          </div>
+          <div className="side-nav">
+            <SideNav onNavClick={handleNavClick} selectedOption={selectedTimeframe} />
+          </div>
         </div>
       </div>
-    </div>
-     
     </div>
   );
 };
