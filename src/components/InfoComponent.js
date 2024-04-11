4f6c3b3faded442ca8c218cc9a8de3a2219eb64a
CHEGEBB <chegephil24@gmail.com>
2024-04-11 11:35:56 +0300

    jsx

--- src/components/InfoComponent.js
+++ src/components/InfoComponent.js
@@ -3,6 +3,12 @@
 import React, { useState } from 'react';
 import Profilepic from "../images/image-jeremy.png";
 import SideNav from "./SideNav";
+import WorkComponent from "./WorkComponent";
+import PlayComponent from "./components/PlayComponent";
+import StudyComponent from "./components/StudyComponent";
+import ExerciseComponent from "./components/ExerciseComponent";
+import SocialComponent from "./components/SocialComponent";
+import SelfCareComponent from "./components/SelfCareComponent";
 import "../InfoComponent.scss";
 
 const InfoComponent = () => {
@@ -14,21 +20,28 @@ const InfoComponent = () => {
 
   return (
     <div className="info-back">
-    <div className="info">
-    <div className="profile">
-        <img src={Profilepic} alt="profile" />
-      </div>
-      <div className="info-content">
-        <div className="profile-info">
-          <h1>Report for</h1>
-          <h2>Jeremy Robson</h2>
+      <div className="info">
+        <div className="profile">
+          <img src={Profilepic} alt="profile" />
         </div>
-        <div className="side-nav">
-          <SideNav onNavClick={handleNavClick} selectedOption={selectedTimeframe} />
+        <div className="info-content">
+          <div className="profile-info">
+            <h1>Report for</h1>
+            <h2>Jeremy Robson</h2>
+          </div>
+          <div className="side-nav">
+            <SideNav onNavClick={handleNavClick} selectedOption={selectedTimeframe} />
+          </div>
         </div>
       </div>
-    </div>
-     
+      {/* Pass selectedTimeframe prop to each card component */}
+      <WorkComponent selectedTimeframe={selectedTimeframe} />
+      <PlayComponent selectedTimeframe={selectedTimeframe} />
+      <StudyComponent selectedTimeframe={selectedTimeframe} />
+      <ExerciseComponent selectedTimeframe={selectedTimeframe} />
+      <SocialComponent selectedTimeframe={selectedTimeframe} />
+      <SelfCareComponent selectedTimeframe={selectedTimeframe} />
+      console.log(<SelfCareComponent selectedTimeframe={selectedTimeframe} />);
     </div>
   );
 };
