92fd90da3b68d5dbe3f5decb17cdcdfb4cfb72b0
CHEGEBB <chegephil24@gmail.com>
2024-04-11 09:44:44 +0300

    scss

---
+++ src/components/PlayComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import EllipsIcon from "../images/icon-ellipsis.svg";
+import "../App";
+
+const PlayComponent = () => {
+    return ( 
+        
+     );
+}
+ 
+export default PlayComponent;
