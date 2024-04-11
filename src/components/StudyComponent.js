bc1e0a14b5762408a4dac9d69b2b0bda33010690
CHEGEBB <chegephil24@gmail.com>
2024-04-11 09:53:36 +0300

    jsx

--- src/components/StudyComponent.js
+++ src/components/StudyComponent.js
@@ -1,3 +1,42 @@
 import {useState, useEffect} from "react";
 import EllipsIcon from "../images/icon-ellipsis.svg";
 import "../App";
+
+const StudyComponent = () => {
+    const [studyData, setStudyData] = useState({});
+    useEffect(() => {
+        // Fetch data for study activity
+        fetch("http://example.com/data.json")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                const study = data.find((activity) => activity.title === "Study");
+                setStudyData(study);
+            })
+            .catch((error) => console.error("Error fetching study data:", error));
+    }, []);
+    return ( 
+        <div className = "study-back" >
+            <div className = "study-icon" >
+                {/* Your study icon */}
+            </div> 
+            <div className = "study-card" >
+                <div className = "activity" >
+                    <p> Study </p> 
+                    <div className = "ellips" >
+                        <img src = { EllipsIcon } alt = "ellipsis" />
+                    </div> 
+                </div> 
+                <div className = "hours" >
+                    <p> 7hrs </p>
+                </div> 
+            </div> 
+        </div>
+     );
+}
+ 
+export default StudyComponent;
