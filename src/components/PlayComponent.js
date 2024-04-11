655c5c77c8eea34feb537c609d01bb3c4b5f95c8
CHEGEBB <chegephil24@gmail.com>
2024-04-11 09:44:57 +0300

    scss

--- src/components/PlayComponent.js
+++ src/components/PlayComponent.js
@@ -4,7 +4,23 @@ import "../App";
 
 const PlayComponent = () => {
     return ( 
-        
+        <div className = "play-back" >
+            <div className = "play-icon" >
+                {/* Your play icon */}
+            </div> 
+            <div className = "play-card" >
+                <div className = "activity" >
+                    <p> Play </p> 
+                    <div className = "ellips" >
+                        <img src = { EllipsIcon } alt = "ellipsis" />
+                    </div> 
+                </div> 
+                <div className = "hours" >
+                    <p> 1 hrs </p> 
+                </div> 
+            </div> 
+        </div>
+
      );
 }
  
