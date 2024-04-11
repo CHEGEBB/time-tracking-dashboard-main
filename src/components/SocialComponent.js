9acbb6053c5f7288651fc7d56a291b61e223baf0
CHEGEBB <chegephil24@gmail.com>
2024-04-11 10:07:43 +0300

    jsx

--- src/components/SocialComponent.js
+++ src/components/SocialComponent.js
@@ -1,3 +1,12 @@
 import {useState, useEffect} from "react";
 import EllipsIcon from "../images/icon-ellipsis.svg";
 import '../App';
+
+const SocialComponent = () => {
+    const [socialData, setSocialData]=useState({});
+    return ( 
+
+     );
+}
+ 
+export default SocialComponent;
