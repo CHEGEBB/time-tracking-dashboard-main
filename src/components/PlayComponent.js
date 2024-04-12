e3ede2bcb6de7b813db281a69b8f17879684f347
CHEGEBB <chegephil24@gmail.com>
2024-04-12 11:10:18 +0300

    jsx

--- src/components/PlayComponent.js
+++ src/components/PlayComponent.js
@@ -2,26 +2,24 @@ import React, { useState, useEffect } from "react";
 import EllipsIcon from "../images/icon-ellipsis.svg";
 import PlayIcon from "../images/icon-play.svg";
 
-const PlayComponent = () => {
-  const [playData, setPlayData] = useState({});
+const PlayComponent = ({ selectedTimeframe }) => {
+  const [playData, setPlayData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLoading(true);
     fetch("http://localhost:8000/1")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
+      .then((res) => res.json())
       .then((data) => {
-        const play = data.find((activity) => activity.title === "Play");
-        setPlayData(play);
+        const timeframeData = data.timeframes[selectedTimeframe.toLowerCase()];
+        setPlayData(timeframeData);
         setLoading(false);
       })
-      .catch((error) => console.error("Error fetching play data:", error));
-  }, []);
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
+  }, [selectedTimeframe]);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -40,12 +38,10 @@ const PlayComponent = () => {
           </div>
         </div>
         <div className="hours">
-          <p>
-            {playData.timeframes &&
-              playData.timeframes.weekly &&
-              playData.timeframes.weekly.current}{" "}
-            hrs
-          </p>
+          <p>{playData && playData.current} hrs</p>
+          <div className="previous">
+            <p>Last {selectedTimeframe.charAt(0).toUpperCase() + selectedTimeframe.slice(1)} - {playData && playData.previous} hrs</p>
+          </div>
         </div>
       </div>
     </div>
